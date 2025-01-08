935ea991652cffd74a0ee8cf6a8be3252fa308c2
ETKim <ket0825@gmail.com>
2025-01-08 19:19:11 +0900

    Revert "feature: Add Transform and Load sequence"
    This reverts commit 559febd36e993badb13451899b6360cae32894f3.

--- missions/W1/M3/utils/sql/create_gdp.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS gdp (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    Country varchar(127) NOT NULL,
-    Region varchar(127) NOT NULL, -- country가 추가되지 않고, 매핑 테이블의 변화가 없다는 가정이라면 NOT NULL
-    GDP_USD_billion float,
-    Year int,
-    Type varchar(63) NOT NULL,
-    CreatedAt DATETIME DEFAULT CURRENT_TIMESTAMP
-);
