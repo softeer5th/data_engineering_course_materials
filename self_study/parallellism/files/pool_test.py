e68e25540644d3df630019d2d7abd07f693e4c6f
reudekx <reudekx@gmail.com>
2025-01-10 18:22:41 +0900

    feat: update self-study contents

--- self_study/parallellism/files/pool_test.py
+++ self_study/parallellism/files/pool_test.py
@@ -1,19 +1,21 @@
-from multiprocessing import Pool
 import time
+from multiprocessing import Pool
+
 
 def slow_square(x):
     time.sleep(1)  # 작업이 오래 걸리는 것을 시뮬레이션
     return x * x
 
+
 def main():
     numbers = [1, 2, 3, 4, 5]
-    
+
     print("일반적인 처리 시작")
     start = time.time()
     regular_result = [slow_square(x) for x in numbers]
     print(f"일반 처리 결과: {regular_result}")
     print(f"소요 시간: {time.time() - start:.2f}초\n")
-    
+
     print("병렬 처리 시작")
     start = time.time()
     with Pool() as pool:
@@ -21,5 +23,6 @@ def main():
     print(f"병렬 처리 결과: {list(parallel_result)}")
     print(f"소요 시간: {time.time() - start:.2f}초")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- self_study/parallellism/files/queue_test.py
+++ self_study/parallellism/files/queue_test.py
@@ -1,7 +1,8 @@
 import multiprocessing as mp
-from queue import Empty
-import time
 import random
+import time
+from queue import Empty
+
 
 def process_task(queue: mp.Queue, process_id: int) -> None:
     """
@@ -13,43 +14,45 @@ def process_task(queue: mp.Queue, process_id: int) -> None:
 
         if data is None:
             # None이면 종료
-            print(f'프로세스 {process_id}: 작업 완료')
+            print(f"프로세스 {process_id}: 작업 완료")
             break
 
         # 처리 시작을 알림
-        print(f'프로세스 {process_id}: 값 {data} 처리 시작')
-        
+        print(f"프로세스 {process_id}: 값 {data} 처리 시작")
+
         # 데이터 값/10 초 만큼 대기
-        time.sleep(data/10)
-        
+        time.sleep(data / 10)
+
         # 처리 완료를 알림
-        print(f'프로세스 {process_id}: 값 {data} 처리 완료')
+        print(f"프로세스 {process_id}: 값 {data} 처리 완료")
+
 
 def main():
     # 프로세스 간 공유할 큐 생성
     task_queue = mp.Queue()
-    
+
     # 1~10 사이의 랜덤한 값 10개를 큐에 넣기
     numbers = random.sample(range(1, 11), 10)
     numbers.extend([None] * 4)
     for num in numbers:
         task_queue.put(num)
-    
+
     # 4개의 프로세스 생성
     processes = []
     for i in range(4):
         p = mp.Process(target=process_task, args=(task_queue, i))
         processes.append(p)
-        print(f'프로세스 {i} 시작')
+        print(f"프로세스 {i} 시작")
         p.start()
-    
+
     # 모든 프로세스가 종료될 때까지 대기
     print("모든 프로세스가 종료될 때까지 대기합니다.")
     for p in processes:
-        print(f'프로세스 {p.pid} 대기 중')
+        print(f"프로세스 {p.pid} 대기 중")
         p.join()
-    
+
     print("모든 작업이 완료되었습니다.")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
