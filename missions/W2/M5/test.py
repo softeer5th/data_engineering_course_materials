14fa0042b0e4b5fdb3b7cf4dc508555df62cb7e1
sumin <cleonn7878@gmail.com>
2025-01-13 18:43:05 +0900

    Revert "Feat: w2 m4까지 구현"
    This reverts commit 203515d36a267bb3bea1e932f710812ed5a80360.

--- missions/W2/M5/test.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-data= [[[1,2,3],0,['a','b','c']],[4,[],3],[5,2,['x','y','z']]]
-idx = ['row1','row2','row3']
-col = ['col1','col2','col3']
-df = pd.DataFrame(data = data, index = idx, columns = col)
-print(df)
-print(df.explode('col3'))
