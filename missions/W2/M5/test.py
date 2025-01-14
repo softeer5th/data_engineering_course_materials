6fb9494ca817b4c7548538e1ccd49ca64e54edc6
sumin <cleonn7878@gmail.com>
2025-01-14 18:31:44 +0900

    Feat: M5까지 구현

--- missions/W2/M5/test.py
+++ missions/W2/M5/test.py
@@ -1,7 +1,27 @@
 import pandas as pd
-data= [[[1,2,3],0,['a','b','c']],[4,[],3],[5,2,['x','y','z']]]
-idx = ['row1','row2','row3']
-col = ['col1','col2','col3']
-df = pd.DataFrame(data = data, index = idx, columns = col)
-print(df)
-print(df.explode('col3'))
+import nltk
+import time
+from nltk.tokenize import word_tokenize
+
+def myFilter(s:str)-> str:
+    if s[0] == '@':
+        return ''
+    elif len(s) == 1:
+        return ''
+    else:
+        return s.strip("!@#$%^&*()-=;'\",.")
+
+def mySplit(text:str)->list:
+    res = []
+    res = list(filter(lambda x: x != '', map(myFilter, (text.split()))))
+    return res
+
+if __name__ == '__main__':
+    s = time.time()
+    path = './missions/W2/M5/data/'
+    data_name = 'training.1600000.processed.noemoticon.csv'
+    full_name = path + data_name
+    df = pd.read_csv(full_name, usecols=['text'], nrows=10)
+    e = time.time()
+    print(df['text'].apply(mySplit))
+    print(f'elapsed: {s - e}')
