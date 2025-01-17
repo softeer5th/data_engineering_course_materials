2de42f70d5b2ad99713db270b400f974ea1a5630
openkmj <openkmj@gmail.com>
2025-01-17 18:56:59 +0900

    fix

--- missions/W2/M4.py
+++ missions/W2/M4.py
@@ -1,53 +1,114 @@
 from multiprocessing import Queue, Process, Event
 import time
+from queue import Empty, Full
+
+TOTAL_TASK = 100
+NUM_PRODUCER = 2
+PRODUCE_DELAY = 0.01
+NUM_CONSUMER = 4
+CONSUME_DELAY = 0.001
+NUM_PRINTER = 1
 
 
-def provide_task(in_queue: Queue, count: int):
+def produce_task(in_queue: Queue, idx: int, count: int):
     for i in range(count):
-        in_queue.put(f"Task no {i}")
-        print(f"Task no {i}")
+        try:
+            in_queue.put_nowait(f"Task no {idx} {i}")
+            time.sleep(PRODUCE_DELAY)
+        except Full:
+            print(f"ERROR: Queue is full. Task no {i} is not added in Producer-{idx}")
+    print(f"INFO: Producer-{idx} is done.")
 
 
 def consume_task(in_queue: Queue, out_queue: Queue, idx: int, stop_event):
-    while not in_queue.empty() or not stop_event.is_set():
+    tmp = []
+    count = 0
+    while not (in_queue.empty() and stop_event.is_set()):
         try:
             task = in_queue.get_nowait()
-            time.sleep(0.5)
-            out_queue.put(f"{task} is done by Process-{idx}")
+            count += 1
+            time.sleep(CONSUME_DELAY)
+
+            while True:
+                try:
+                    out_queue.put_nowait(f"{task} is done by Process-{idx}")
+                    break
+                except Full:
+                    print(
+                        f"WARN: Queue is full. Task-{task} is not added in Process-{idx}. Retry has been scheduled."
+                    )
+                    tmp.append(task)
+                    break
         except:
             continue
 
+    for task in tmp:
+        print(f"INFO: Retry to add task-{task} in Process-{idx}")
+        try:
+            out_queue.put_nowait(f"{task} is done by Process-{idx}")
+        except Full:
+            print(f"ERROR: Queue is full. Task-{task} is not added in Process-{idx}")
 
+    print(f"INFO: Consumer-{idx} is done.")
+    print(f"INFO: Consumer-{idx} Fairness: {count}")
+
+
+def print_task(queue: Queue, idx: int, stop_event):
+    while not (queue.empty() and stop_event.is_set()):
+        try:
+            task = queue.get_nowait()
+            print(f"PRINT({idx}): {task}")
+        except Empty:
+            continue
+    print(f"INFO: Printer-{idx} is done.")
+
+
+# 1. Producer가 여러개인 경우
+# 2. Producer가 다 끝나기 전부터 Consumer를 시작하고 싶다.
 def main():
     tasks_to_accomplish = Queue()
     tasks_that_are_done = Queue()
-    stop_event = Event()
+    producer_end_event = Event()
+    consumer_end_event = Event()
 
-    providers = [
-        Process(target=provide_task, args=(tasks_to_accomplish, 100)) for i in range(2)
+    producer = [
+        Process(
+            target=produce_task,
+            args=(tasks_to_accomplish, i, int(TOTAL_TASK / NUM_PRODUCER)),
+        )
+        for i in range(NUM_PRODUCER)
     ]
-    [p.start() for p in providers]
-
     consumers = [
         Process(
             target=consume_task,
-            args=(tasks_to_accomplish, tasks_that_are_done, i, stop_event),
+            args=(tasks_to_accomplish, tasks_that_are_done, i, producer_end_event),
         )
-        for i in range(4)
+        for i in range(NUM_CONSUMER)
     ]
-    [p.start() for p in consumers]
+    printer = [
+        Process(target=print_task, args=(tasks_that_are_done, i, consumer_end_event))
+        for i in range(NUM_PRINTER)
+    ]
 
-    [p.join() for p in providers]
-    # end signal
-    stop_event.set()
+    for p in printer:
+        p.start()
 
-    [p.join() for p in consumers]
+    for p in consumers:
+        p.start()
 
-    while not tasks_that_are_done.empty():
-        try:
-            print(tasks_that_are_done.get_nowait())
-        except:
-            break
+    for p in producer:
+        p.start()
+
+    for p in producer:
+        p.join()
+    producer_end_event.set()
+
+    for p in consumers:
+        p.join()
+    consumer_end_event.set()
+
+    for p in printer:
+        p.join()
 
 
 if __name__ == "__main__":
