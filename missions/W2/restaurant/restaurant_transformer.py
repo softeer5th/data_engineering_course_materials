06dd7a831fe02d3ef98d48c9899247913cb6f344
김태현 <speowo@gimtaehyeon-ui-MacBookPro.local>
2025-01-17 18:47:01 +0900

    feat: get_id_rating_reviews_from_id_text

--- missions/W2/restaurant/restaurant_transformer.py
+++ missions/W2/restaurant/restaurant_transformer.py
@@ -4,24 +4,33 @@ from bs4 import BeautifulSoup
 # 필요한 거
 # 가게 아이디 + 리뷰
 
+# parameter:
+
 def get_reviews_from_json(reviews_json):
     ids_texts = reviews_json['restaurants'] # [{id:~ restaurant:~}, ~~ 반복인 리스트]
     ids_reviews = []
 
     for id_text in ids_texts:  # 리스트 내의 딕셔너리
-        id = id_text['id']
-        text = id_text['text']
-
-        reviews = []
-        soup = BeautifulSoup(text, 'lxml')
-        comments = soup.find_all('p', class_= 'txt_comment')
-        for comment in comments:
-            span = comment.find('span').text 
-            if span:
-                reviews.append(span)
-
-        # reviews에 comments text 다 들어가 있다.
-        # {'id' = id, 'comments' = [comments]}
-        ids_reviews.append({'id': id, 'reviews': reviews})
+        id_rating_reviews = get_id_reviews_from_id_text(id_text)
+        ids_reviews.append(id_rating_reviews)
 
     return ids_reviews
+
+def get_id_reviews_from_id_text(id_text):
+    id = id_text['id']
+    text = id_text['text']
+
+    reviews = []
+    soup = BeautifulSoup(text, 'lxml')
+    comments = soup.find_all('p', class_= 'txt_comment')
+    for comment in comments:
+        span = comment.find('span').text 
+        if span:
+            reviews.append(span)
+
+    rating_tag = soup.find(class_='color_b')
+    if rating_tag is None:
+        return
+    rating = rating_tag.text
+
+    return {'id': id, 'rating': rating, 'reviews': reviews}
