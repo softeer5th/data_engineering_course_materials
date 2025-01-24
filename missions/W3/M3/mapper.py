39a588971dd30918fa0b4f29bf46821432317e68
seonwoonam <potatomoney716@gmail.com>
2025-01-24 18:37:34 +0900

    feat : mapreduce 파이썬으로 구현

---
+++ missions/W3/M3/mapper.py
@@ -0,0 +1,11 @@
+import sys 
+import re 
+#\t를 기준으로 키와 값 분리
+def mapper():
+    for line in sys.stdin:
+        line = line.strip()
+        words = re.findall(r'\w+', line.lower())
+        for word in words:
+            print("{word}\t1".format(word = word))
+
+mapper()
---
+++ missions/W3/M3/reducer.py
@@ -0,0 +1,16 @@
+import sys 
+
+def reducer():
+    current_word = None 
+    total = 0 
+
+    for line in sys.stdin:
+        word, count = line.split('\t')
+        if word != current_word:
+            if current_word:
+                print('{current}\t{total}'.format(current_word=current_word, total = total))
+            current_word = word 
+            total = 0
+        total += int(count)
+
+reducer()
